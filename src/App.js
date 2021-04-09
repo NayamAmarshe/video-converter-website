89a97ccf89a8fd756542e38ef2a274f0c77c2faf
Kashyyk <mayankvallah@laptop.com>
2021-04-09 22:21:37 +0530

    Adding Initial WA Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
+const ffmpeg = createFFmpeg({ log: true });
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [ready, setReady] = useState(false);
+	const [video, setVideo] = useState();
+	const [gif, setGif] = useState();
 
+	const load = async () => {
+		await ffmpeg.load();
+		setReady(true);
+	};
+
+	const convertToGif = async () => {
+		ffmpeg.FS("writeFile", "test.mp4", await fetchFile(video));
+		await ffmpeg.run(
+			"-i",
+			"test.mp4",
+			"t",
+			"-2.5",
+			"-ss",
+			"-f",
+			"gif",
+			"out.gif"
+		);
+
+		const data = ffmpeg.FS("readFile", "out.gif");
+		const url = URL.createObjectURL(
+			new Blob([data.buffer], { type: "image / gif" })
+		);
+		setGif(url);
+	};
+
+	useEffect(() => {
+		load();
+	}, []);
+
+	return ready ? (
+		<div className="App">
+			{video && (
+				<video
+					controls
+					width="250"
+					src={URL.createObjectURL(video)}
+				></video>
+			)}
+			<input type="file" onChange={(e) => setVideo(e.target.files[0])} />
+
+			<h3>Result</h3>
+			<button onClick={convertToGif}>Convert</button>
+			{gif && <img src={gif} alt="gif" />}
+		</div>
+	) : (
+		<p>Loading...</p>
+	);
+}
 export default App;
